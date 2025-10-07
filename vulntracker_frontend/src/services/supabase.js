312ee93d088815700f46d4eaf5d663983f2db8f3
Fayol-01 <fayolaj12@gmail.com>
2025-10-07 12:08:08 +0530

    Update supabase.js

--- vulntracker_frontend/src/services/supabase.js
+++ vulntracker_frontend/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rsgbbkyiktkmkqcqirpy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzZ2Jia3lpa3RrbWtxY3FpcnB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNDYwMjYsImV4cCI6MjA3MzkyMjAyNn0.9CWQxDvVOV59urjFjpSfva9n4oY_iP61mvayiRltRsk';
+const supabaseUrl = '';
+const supabaseKey = '';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
